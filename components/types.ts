7d7e961d840cdb9cf07a769fff11fd0cd2b11848
hellopadu003 <hellopadu003@gmail.com>
2026-01-16 18:47:07 +0600

    Add files via upload

---
+++ components/types.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  title: string;
+  category: string;
+  image: string;
+  desc: string;
+  tags: string[];
+}
+
+export interface FaqItem {
+  question: string;
+  answer: string;
+}
